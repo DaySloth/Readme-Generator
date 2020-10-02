7665b8bd1177ec2220fb61394bd2f548ca363704
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-02 07:09:46 -0400

    added questions for for inquirer

--- index.js
+++ index.js
@@ -6,13 +6,54 @@ const inquirer = require("inquirer");
 const util = require("util");
 
 
+
 const questions = [
     {
         type: "input",
         name: "title",
         message: "What is the title of your project?"
     },
-
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project?"
+    },
+    {
+        type: "input",
+        name: "tableOfContent",
+        message: "Provide a table of content"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How would a user INSTALL your program?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How would a user USE your program?"
+    },
+    {
+        type: "list",
+        name: "license",
+        choices: ["MIT", "Apache", "ISC"],
+        message: "Choose a license for your project"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can a user CONTRIBUTE to the project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How can a user TEST your project?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What questions are left after this project?"
+    },
 ];
 
 // function to write README file
@@ -21,7 +62,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-    inquirer.prompt(questions[0]);
+    const answers = inquirer.prompt(questions);
 }
 
 // function call to initialize program
