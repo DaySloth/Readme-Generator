cc44ca211de686178d15681939e4bfddf7e071e3
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-02 06:53:18 -0400

    added inquirer prompt for questions

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // array of questions for user
+//title, description, table of content, installation, usage, license
+//contributing, tests and questions
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+
+
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
 
 ];
 
@@ -9,7 +21,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions[0]);
 }
 
 // function call to initialize program
