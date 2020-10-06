288454584864dd676504967f9514f3f2af5a2bcc
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-06 15:24:52 -0500

    Update index.js
    added readme coding

--- index.js
+++ index.js
@@ -5,56 +5,94 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
 
+function promptQuestions() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Describe your project?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "How would a user INSTALL your program?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "How would a user USE your program?"
+        },
+        {
+            type: "list",
+            name: "license",
+            choices: ["MIT", "Apache", "ISC"],
+            message: "Choose a license for your project"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "How can a user CONTRIBUTE to the project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "How can a user TEST your project?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What questions are left after this project?"
+        },
+    ]);
+};
 
+function generateREADME(data){
+    return `
+    #${data.title}
 
-const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Describe your project?"
-    },
-    {
-        type: "input",
-        name: "tableOfContent",
-        message: "Provide a table of content"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "How would a user INSTALL your program?"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "How would a user USE your program?"
-    },
-    {
-        type: "list",
-        name: "license",
-        choices: ["MIT", "Apache", "ISC"],
-        message: "Choose a license for your project"
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "How can a user CONTRIBUTE to the project?"
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "How can a user TEST your project?"
-    },
-    {
-        type: "input",
-        name: "questions",
-        message: "What questions are left after this project?"
-    },
-];
+    ## Description
+    
+    ${data.description}
+
+    ## Table of Content
+
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [License](#license)
+    
+    ## Installation
+
+    ${data.installation}
+
+    ## Usage
+
+    ${data.usage}
+
+    ## Contribution
+
+    ${data.contributing}
+
+    ## Tests
+
+    ${data.tests}
+
+    ## Questions
+
+    ${data.questions}
+    
+    ## License
+
+    ${data.license}
+    
+    `
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -62,7 +100,24 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-    const answers = inquirer.prompt(questions);
+    promptQuestions().then(function(answers){
+        let providedAnswers = answers;
+        console.log(providedAnswers);
+        if(providedAnswers.license === "Apache"){
+            providedAnswers.license = "[Apache](http://www.apache.org/licenses/LICENSE-2.0)";
+        }else if(providedAnswers.license === "MIT"){
+            providedAnswers.license = "[MIT](https://choosealicense.com/licenses/mit/)";
+        }else{
+            providedAnswers.license = "[ISC](https://choosealicense.com/licenses/isc/)";
+        };
+
+        console.log(providedAnswers);
+
+        const README = generateREADME(providedAnswers);
+    })
+    .then(function(){
+
+    })
 }
 
 // function call to initialize program
