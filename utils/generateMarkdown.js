70f0fa5ca68f79615c13c1460fa5a1f49217e23a
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-07 10:20:08 -0500

    put code into generateMarkdown js file to create markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,47 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
-`;
-}
+![Image of Project](${data.imageAddress})
+
+## Description
+
+${data.description}
+
+## Table of Content
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.questions}
+
+## License
+
+Licensed under the ${data.license} license
+
+`
+};
 
 module.exports = generateMarkdown;
